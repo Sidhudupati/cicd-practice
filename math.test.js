76617596fb41939cf8b32ef14544e39e113a6238
Siddharth <sdupati4@gmail.com>
2026-06-04 15:53:33 +0530

    Figured it

--- math.test.js
+++ math.test.js
@@ -1,7 +1,7 @@
 const { add, multiply } = require('./math');
 
 test('adds 2 + 3 to equal 5', () => {
-  expect(add(2, 3)).toBe(55);
+  expect(add(2, 3)).toBe(5);
 });
 
 test('multiplies 4 * 5 to equal 20', () => {
